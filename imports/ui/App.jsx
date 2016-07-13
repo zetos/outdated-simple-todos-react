6aa721a631a77bd1bb17062e4ead3d544cc840ae
Zetos <cesar_manoel@hotmail.com>
2016-07-12 22:55:48 -0300

    user accounts added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
- 
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+
 // App component - represents the whole app
 class App extends Component {
   constructor(props) {
@@ -62,7 +63,9 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
- 
+          
+          <AccountsUIWrapper />
+          
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
