d91fc98d08ddd29aa43e3a250a33b1ba548f3fcd
Zetos <cesar_manoel@hotmail.com>
2016-07-12 23:54:49 -0300

    private and public tasks implemented

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,8 +6,14 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
   // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
   });
 }
 
@@ -37,5 +43,18 @@ Meteor.methods({
     check(setChecked, Boolean);
  
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+ 
+    const task = Tasks.findOne(taskId);
+ 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
@@ -11,11 +12,18 @@ export default class Task extends Component {
 	deleteThisTask() {
     	Meteor.call('tasks.remove', this.props.task._id);
 	}
- 
+
+	togglePrivate() {
+	    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+	}
+
   render() {
   	// Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
     return (
         <li className={taskClassName}>
@@ -29,7 +37,13 @@ export default class Task extends Component {
 	          checked={this.props.task.checked}
 	          onClick={this.toggleChecked.bind(this)}
 	        />
-	 
+
+	        { this.props.showPrivateButton ? (
+	          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+	            { this.props.task.private ? 'Private' : 'Public' }
+	          </button>
+	        ) : ''}
+ 	 
 	        <span className="text">
 	          <strong>{this.props.task.username}</strong>: {this.props.task.text}
 	        </span>
@@ -42,4 +56,5 @@ Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
 };
